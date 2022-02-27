824f249de2b6a5c32019f795403b77b19b7ff384
Paola Ayala <paolaaz235@gmail.com>
2022-02-27 10:24:47 -0500

    Build the game and test
    Write code for the game and test the game to see if it works

--- src/app.js
+++ src/app.js
@@ -22,6 +22,20 @@ const suits = ['hearts', 'spades', 'diamonds', 'clubs'];
 while (confirm('Would you like to play the game?')) {
   // Put your code in this while loop
   // Remember to use prompt and alert
+  alert('Ready to guess a suit?');
+  const random = getRandomInt(0, 4);
+  console.log(random);
+  const result = prompt(
+    'Which would you like to pick: hearts, spades, diamonds, or clubs?'
+  );
+  const input = result.toLowerCase();
+  const type = suits[random];
+  console.log(input);
+  if (input === type) {
+    alert('Congratulations, you won!');
+  } else if (input !== type) {
+    alert('Oh no, you lost!');
+  }
 }
 
 farewell.innerHTML = '<h1>Thanks for playing!<h1>';
